464bf91ec32671ed5d587edc5961972524f0334d
PeterVincent96 <peter.vincent.14@ucl.ac.uk>
2020-10-28 13:34:28 +0000

    Prepare for HPC simulations

--- plotting_functions/plot_trial.m
+++ plotting_functions/plot_trial.m
@@ -9,7 +9,11 @@ num_trials = length(mdp);
 
 prior_belief    =  zeros(1,num_trials);
 posterior_belief=  zeros(1,num_trials);
+prior_card      =  zeros(1,num_trials);
+posterior_card  =  zeros(1,num_trials);
+
 correct_post_inf=  zeros(1,num_trials);
+correct_card_inf=  zeros(1,num_trials);
 affect_state    =  zeros(1,num_trials);
 fig1 = figure('color','w');
 max_pos = length(mdp);
@@ -67,8 +71,26 @@ for trial = 1:num_trials
     cur_trial = mdp(trial);
     outcomes   = cur_trial.o;
     inf_over_ad=cur_trial.xn{1,1};
+    inf_over_card = cur_trial.xn{1,2};
+    
+    prior_card(1,trial) = inf_over_card(1,1,3,2);
+    posterior_card(1,trial) = inf_over_card(1,1,3,3);
+    
+    card_choice = prior_card(1,trial) >= 0.5;
+    final_card_choice = posterior_card(1,trial) >= 0.5;
+    if (outcomes(2,3) == 1) && (card_choice == final_card_choice)
+        correct_card_inf(1,trial) = 1;
+    elseif (outcomes(2,3) == 2) && (card_choice ~= final_card_choice)
+        correct_card_inf(1,trial) = 1;
+    else
+        correct_card_inf(1,trial) = 0;
+    end
+        
+    
+    
     prior_belief(1,trial) = inf_over_ad(end,1,2,2);
     posterior_belief(1,trial) = inf_over_ad(end,1,2,3);
+    
     if (outcomes(1,2) == outcomes(4,3)) && (outcomes(2,3) == 1)
         trustworthy_val = 1;
     elseif (outcomes(1,2) ~= outcomes(4,3)) && (outcomes(2,3) == 1)
@@ -93,6 +115,7 @@ end
 % Calculate performance metrics
 per_correct = (sum(feedback_traj == 1)/length(feedback_traj)) * 100;
 per_good_inf=(1-(sum(correct_post_inf == 1)/length(correct_post_inf)))*100;
+per_good_card_inf = (1 - (sum(correct_card_inf == 1)/length(correct_card_inf)))*100;
 
 plot(1:num_trials,decision_traj,'LineWidth',2,'Color','k');
 ax = gca;
@@ -132,7 +155,7 @@ for trial = 1:num_trials
     rectangle(ax2,'Position',[trial-0.5,0,1,2],...
         'FaceColor',trustworthy_colour,'EdgeColor','none');
     if correct_post_inf(1,trial) == 0
-        rectangle(ax2,'Position',[trial-0.5,0.5,1,0.6],...
+        rectangle(ax2,'Position',[trial-0.5,0.5,1,0.5],...
             'FaceColor',poor_inf_col,'EdgeColor','none');
     end
 end
@@ -149,7 +172,7 @@ text(ax2,num_trials+1,0,false_message,'Color',poor_inf_col);
 ax2.XLabel.String = "Trial number";
 ax2.YLabel.String = "Probability";
 hold('off');
-
+%%
 %%%% Make the third figure to illustrate the affective state of the agent
 %%%% with smoothing across some number of trials (10 in this case)
 smoothing_kernel = 10;
@@ -172,52 +195,101 @@ text(ax3,0,1.5,"Arousal state",'HorizontalAlignment','right');
 text(ax3,num_trials+1,2,"High arousal",'Color','k');
 text(ax3,num_trials+1,1,"Low arousal",'Color','k');
 hold(ax3,'off');
+
+%% Make figure to plot the inferences over the card
+
+fig4 = figure('color','w');
+hold on
+xlim([0.5 (num_trials+0.5)]);
+ylim([0 1])
+ax4 = gca;
+for trial = 1:num_trials
+    if feedback_traj(1,trial) == 1
+        feedback_colour = corr_feedback_col;    
+    else
+        feedback_colour = false_feedback_col;
+    end
+    rectangle(ax4,'Position',[trial-0.5,0,1,2],...
+        'FaceColor',feedback_colour,'EdgeColor','none');
+    if correct_card_inf(1,trial) == 0
+        rectangle(ax4,'Position',[trial-0.5,0.5,1,0.5],...
+            'FaceColor',poor_inf_col,'EdgeColor','none');
+    end
+end
+
+plot(ax4,1:num_trials,prior_card,'color','k','LineWidth',2);
+plot(ax4,1:num_trials,posterior_card,'.','color',posterior_col,'MarkerSize',15);
+%posterior_message = sprintf('Posterior\nBelief');
+%prior_message     = sprintf('Prior\nBelief');
+%text(ax4,num_trials+1,0.5,prior_message,'Color','k');
+text(ax4,num_trials+1,0.25,strcat(num2str(per_good_card_inf),'%'),'Color',poor_inf_col);
+
+%text(ax4,num_trials+1,1,posterior_message,'Color',posterior_col);
+ax4.XLabel.String = "Trial number";
+ax4.YLabel.String = "Probability";
+hold('off');
+
 %% Make the combined figure without arousal
 fig1_axes = findobj('Parent',fig1,'Type','axes');
 fig2_axes = findobj('Parent',fig2,'Type','axes');
+fig3_axes = findobj('Parent',fig4,'Type','axes');
 h1_ax = fig1_axes(1);
 h2_ax = fig2_axes(1);
+h3_ax = fig3_axes(1);
 figure('Color','w');
-sub_ax1 = subplot(5,1,1:3);
+sub_ax1 = subplot(7,1,1:3);
 set(sub_ax1,'Box','off');
 sub_ax1.YAxis.Visible = 'off'; 
 sub_ax1.XAxis.Visible = 'off';
 sub_ax1.XLim = [0.5 (num_trials+0.5)];
 sub_ax1.YLim = [-max_total max_total];
 
-sub_ax2 = subplot(5,1,4:5);
+sub_ax2 = subplot(7,1,4:5);
 sub_ax2.XLim = [0.5 (num_trials+0.5)];
 sub_ax2.YLim = [0 1];
-sub_ax2.XLabel.String = "Trial number";
 sub_ax2.YLabel.String = "Probability";
+
+sub_ax3 = subplot(7,1,6:7);
+sub_ax3.XLim = [0.5 (num_trials+0.5)];
+sub_ax3.YLim = [0 1];
+sub_ax3.YLabel.String = "Probability";
+sub_ax3.XLabel.String = "Trial number";
 
 copyobj(get(h1_ax,'Children'),sub_ax1);
 copyobj(get(h2_ax,'Children'),sub_ax2);
+copyobj(get(h3_ax,'Children'),sub_ax3);
 %% Make the combined figure with arousal
 fig3_axes = findobj('Parent',fig3,'Type','axes');
-h3_ax = fig3_axes(1);
+h4_ax = fig3_axes(1);
 figure('Color','w');
 
-sub_ax1 = subplot(6,1,1:3);
+sub_ax1 = subplot(8,1,1:3);
 set(sub_ax1,'Box','off');
 sub_ax1.YAxis.Visible = 'off'; 
 sub_ax1.XAxis.Visible = 'off';
 sub_ax1.XLim = [0.5 (num_trials+0.5)];
 sub_ax1.YLim = [-max_total max_total];
 
-sub_ax2 = subplot(6,1,4);
+sub_ax2 = subplot(8,1,4);
 set(sub_ax2,'Box','off');
 sub_ax2.YAxis.Visible = 'off'; 
 sub_ax2.XAxis.Visible = 'off';
 sub_ax2.XLim = [0.5 (num_trials+0.5)];
 sub_ax2.YLim = [0.8 2.2];
 
-sub_ax3 = subplot(6,1,5:6);
+sub_ax3 = subplot(8,1,5:6);
 sub_ax3.XLim = [0.5 (num_trials+0.5)];
 sub_ax3.YLim = [0 1];
-sub_ax3.XLabel.String = "Trial number";
+sub_ax3.XAxis.Visible = 'off';
 sub_ax3.YLabel.String = "Probability";
 
+sub_ax4 = subplot(8,1,7:8);
+sub_ax4.XLim = [0.5 (num_trials + 0.5)];
+sub_ax4.YLim = [0 1];
+sub_ax4.XLabel.String = "Trial number";
+sub_ax4.YLabel.String = "Probability";
+
 copyobj(get(h1_ax,'Children'),sub_ax1);
-copyobj(get(h3_ax,'Children'),sub_ax2);
+copyobj(get(h4_ax,'Children'),sub_ax2);
 copyobj(get(h2_ax,'Children'),sub_ax3);
+copyobj(get(h3_ax,'Children'),sub_ax4);
