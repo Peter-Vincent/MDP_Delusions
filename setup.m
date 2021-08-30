e9be4fe95657d02e98d584c64b68e895cc2b6978
PeterVincent96 <murrieta@hotmail.co.uk>
2021-08-30 21:36:39 +0100

    remove excess/demo/prototype files

--- setup.m
+++
@@ -1,9 +0,0 @@
-%% Initialise paths
-paths = {};
-paths{1} = 'C:\Users\Murri\Documents\MDP_AI\SPM12';
-paths{2} = 'C:\Users\Murri\Documents\MDP_AI\MDP_Delusions\plotting_functions';
-paths{3} = 'C:\Users\Murri\Documents\MDP_AI\MDP_Delusions';
-
-for cur_path = 1:length(paths)
-    addpath(genpath(paths{cur_path}))
-end
