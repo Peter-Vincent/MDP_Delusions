e780bc1217f8bdeff33393e5cd862ade3f28b72c
Peter Vincent <45718646+Peter-Vincent@users.noreply.github.com>
2021-09-01 15:02:00 +0100

    Update analyse_varied_MDPs.m

--- working_model/analyse_varied_MDPs.m
+++ working_model/analyse_varied_MDPs.m
@@ -17,7 +17,6 @@ spm_path   = '/Users/rickadams/Code/SPM/spm12_v7771/';
 save_path  = [save_root save_fold];
 addpath(save_path, code_path, spm_path, plot_path)
 addpath([spm_path 'toolbox/DEM/'])
-addpath(genpath(fun_path))
 
 cd(save_path)
 
